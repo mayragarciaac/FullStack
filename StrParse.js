85740855601ae7926da5ec51e36906b4495c1bd4
Mayra Garcia <mayragarciaac@gmail.com>
2019-10-04 16:28:33 +0100

    Refactoring Kata Game

--- StrParse.js
+++ StrParse.js
@@ -8,66 +8,43 @@ function obtenerNumero(str){
     return Number.isInteger(obtenerNumero(str));
   }
   
+  function ObetenerSuma(numeros){
+    let sum = 0;
+    
+    for(let i=0; i < numeros.length; i++){
+      if(numeros[i] >= 1000)
+        continue;
+  
+      if(EsNumero(numeros[i]))
+        sum+=Number(numeros[i]);
+      else{ 
+        sum= 0; 
+        break; 
+      }
+    }
+    return sum;
+  }
+  
   function kata(str){
     let corteCadena = (str.split('\n'));
     let delimitadores = corteCadena[0].replace("//","");
     let numeros = corteCadena[1].split(delimitadores);
   
-    if(str != ""){
-      if(str.includes(delimitadores)){
-        let sum = 0;
-        for(let i=0; i < numeros.length; i++){
-          if(numeros[i] >= 1000)
-            continue;
+    if(str == "")
+      return 0;
   
-          if(EsNumero(numeros[i]))
-            sum+=Number(numeros[i]);
-          else{ 
-            sum= 0; 
-            break; 
-          }
-        }
+    if(EsNumero(str))
+        return Number(str);
   
-        if(sum != 0)
-          return sum;
-      }
-      else 
-        if(EsNumero(str))
-          return Number(str);
+    if(str.includes(delimitadores)){
+      let sum = ObetenerSuma(numeros);
+      if(sum != 0)
+        return sum;
     }
-   return 0;
-  }
   
-  
-  
-  function kata2(str){
-    let corteCadena = (str.split('\n'));
-    let delimitadores = corteCadena[0].replace("//","");
-    let numeros = corteCadena[1].split(delimitadores);
     
-    if(str != ""){
-      if(str.includes(delimitadores)){
-        let sum = 0;
-        for(let i=0; i < numeros.length; i++){
-          if(numeros[i] >= 1000)
-            continue;
-  
-          if(EsNumero(numeros[i]))
-            sum+=Number(numeros[i]);
-          else{ 
-            sum= 0; 
-            break; 
-          }
-        }
-  
-        if(sum != 0)
-          return sum;
-      }
-      else 
-        if(EsNumero(str))
-          return Number(str);
-    }
    return 0;
   }
+  
   
   console.log(kata("//;\n2;999;-1;1003")); 
